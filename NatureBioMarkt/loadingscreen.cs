bfd4eafc7b3af81a60d5ab8f178e9da29175ffda
LSS\alarab <s.alarab@prologistik.com>
2023-02-13 11:13:14 +0100

    add

---
+++ NatureBioMarkt/loadingscreen.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NatureBioMarkt
+{
+    public partial class loadingscreen : Form
+    {   //3 erstellen eine neue variable
+        private int loadinbarvalue;
+        public loadingscreen()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            loadingbar_timer.Start();//1 timer starten
+        }
+
+        private void progressBar1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void loadingbar_timer_Tick(object sender, EventArgs e)
+        {
+            loadinbarvalue += 1;
+            label2.Text = loadinbarvalue.ToString() + "%";
+            progressBar1.Value = loadinbarvalue;  //2 wenn der timer läuft füllen wi die value.
+            if (loadinbarvalue >= progressBar1.Maximum)
+            {
+                loadingbar_timer.Stop();
+            }
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
